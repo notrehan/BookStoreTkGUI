0d4da1d08f3163a8c82c4c98feadcc651cf8ba82
notrehan <titumax3@gmail.com>
2023-11-30 22:54:15 +0530

    Added Selling page and Sales Record

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter.font import Font
 from PIL import ImageTk, Image
-from functions import addRecord, searchRecord, sell_book, login
+from functions import addRecord, searchRecord, sell_book, login, sell_rec
 from db_functions import logged_in_status
 
 root = tk.Tk()
@@ -24,35 +24,25 @@ def homepage():
 def add_record():
     if logged_in_status() == True:
         addRecord(main_frame, root)
-    # home_frame = tk.Frame(main_frame)
-    # lb = tk.Label(home_frame, text = "adding Page", font = ('Bold', 30))
-    # lb.pack()
-    # home_frame.pack(pady=20)
 
 def sell_record():
-    sell_book(main_frame)
-    # home_frame = tk.Frame(main_frame)
-    
-    # lb = tk.Label(home_frame, text = "selling Page", font = (Helvetica, 30))
-    # lb.pack()
-
-    # home_frame.pack(pady=20)
+    if logged_in_status() == True:
+        sell_book(main_frame)
 
 def search_record():
     if logged_in_status() == True:
         searchRecord(main_frame)
-    # home_frame = tk.Frame(main_frame)
-    
-    # lb = tk.Label(home_frame, text = "Dearchng page Page", font = ('Bold', 30))
-    # lb.pack()
 
-    # home_frame.pack(pady=20)
+def sales_record():
+    if logged_in_status() == True:
+        sell_rec(main_frame)
 
 def hide_all_indicators():
     home_link.configure(bg = nav_bg)
     add_link.configure(bg = nav_bg)
     Sell_link.configure(bg = nav_bg)
     Search_link.configure(bg = nav_bg)
+    Sales_link.configure(bg = nav_bg)
 
 def delete_pages():
     for frame in main_frame.winfo_children():
@@ -71,6 +61,7 @@ options_frame.pack_propagate(False)
 options_frame.configure(width=150, height=600)
 
 
+
 home_link = tk.Button(options_frame, text = 'Home', font = (Helvetica, 12), fg = 'White', bd = 0, bg = nav_bg, activebackground = nav_bg,activeforeground='White' , width=13, command = lambda: indicator(home_link, homepage), anchor='w', padx=15)
 home_link.place(x=0, y=100)
 
@@ -83,11 +74,17 @@ Sell_link.place(x=0, y=170)
 Search_link = tk.Button(options_frame, text = 'Search Books', font = (Helvetica, 12), fg = 'White', bd = 0, bg = nav_bg, activebackground = nav_bg,activeforeground='White' , width=13, command = lambda: indicator(Search_link, search_record), anchor='w', padx=15)
 Search_link.place(x=0, y=205)
 
+Sales_link = tk.Button(options_frame, text = 'Sales Records', font = (Helvetica, 12), fg = 'White', bd = 0, bg = nav_bg, activebackground = nav_bg,activeforeground='White' , width=13, command = lambda: indicator(Sales_link, sales_record), anchor='w', padx=15)
+Sales_link.place(x=0, y=240)
 
 main_frame = tk.Frame(root, bg = primary_bg)
 
 main_frame.pack(side=tk.RIGHT)
 main_frame.pack_propagate(False)
 main_frame.configure(width=1050, height=600)
+
+logo = tk.PhotoImage(file = "Logo.png")
+logo_lb = tk.Label(options_frame, image = logo)
+logo_lb.place(x=25, y=0)
 
 root.mainloop()
